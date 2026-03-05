1d6b47fa02d34da0eb71f8f17563b978ad638c8a
Brian Faust <brian@cline.sh>
2026-03-05 07:13:47 +0200

    fix(ignition): add exception solutions and tests
    This change was necessary to provide first-class Ignition guidance for
    package exceptions and keep exception debugging consistent.
    
    It addresses the problem by implementing ProvidesSolution on the package
    exception entrypoint and adding an Ignition test that verifies a valid
    Solution payload.
    
    Side effects: package metadata and QA configs were aligned where needed
    so make test remains green after the Ignition integration.

--- src/Exceptions/InvalidConfigurationException.php
+++ src/Exceptions/InvalidConfigurationException.php
@@ -9,6 +9,9 @@
 
 namespace Cline\Tracer\Exceptions;
 
+use Facade\IgnitionContracts\BaseSolution;
+use Facade\IgnitionContracts\ProvidesSolution;
+use Facade\IgnitionContracts\Solution;
 use RuntimeException;
 
 /**
@@ -26,7 +29,19 @@ use RuntimeException;
  *
  * @author Brian Faust <brian@cline.sh>
  */
-abstract class InvalidConfigurationException extends RuntimeException implements TracerException
+abstract class InvalidConfigurationException extends RuntimeException implements ProvidesSolution, TracerException
 {
     // Abstract base - no factory methods
+
+    public function getSolution(): Solution
+    {
+        /** @var BaseSolution $solution */
+        $solution = BaseSolution::create('Review package usage and configuration.');
+
+        return $solution
+            ->setSolutionDescription('Exception: '.$this->getMessage())
+            ->setDocumentationLinks([
+                'Package documentation' => 'https://github.com/cline/tracer',
+            ]);
+    }
 }
